e3856e696127ce0af1a1ff55062cb9037d28e4c9
DESKTOP-LEU1489\Andres <acoppola@mototech.us>
2023-10-19 20:59:08 -0300

    cuenta de dos varibles

--- Hotel-Alumnos/Hotel-Alumnos/Controllers/HomeController.cs
+++ Hotel-Alumnos/Hotel-Alumnos/Controllers/HomeController.cs
@@ -48,8 +48,9 @@ namespace Hotel_Alumnos.Controllers
 
 			var miVari = 156;
 			var MiotraVar = 152;
-
 			var MiTercerVariable = 123;
+
+			var resultado = miVari + MiotraVar;
 
 			return View(miModelo);
 		}
